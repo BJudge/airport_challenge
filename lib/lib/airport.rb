6426b0c96e81bd8e0ae5718ce3c25d3b6e3a2fb7
BartJudge <BartJudge@gmail.com>
2019-01-19 20:09:41 +0000

    airport now has multiple plane capacity

--- lib/lib/airport.rb
+++ lib/lib/airport.rb
@@ -1,9 +1,23 @@
 class Airport
+  DEFAULT_CAPACITY = 20
+  attr_reader :capacity, :hanger
+  def initialize(capacity = DEFAULT_CAPACITY)
+    @hanger = []
+    @capacity = capacity
+  end
+
   def land(plane)
-    plane
+    fail "Airport Full!!" if full?
+    @hanger << plane
   end
 
   def takeoff(plane)
+    @hanger.delete(plane)
     "plane #{plane} has now left the airport "
+  end
+
+  private
+  def full?
+    @hanger.count >= @capacity
   end
 end
--- lib/spec/airport_spec.rb
+++ lib/spec/airport_spec.rb
@@ -1,12 +1,23 @@
 require 'airport'
 describe Airport do
+
+  describe '#initialization' do
+    it 'has a variable capacity' do
+        airport = Airport.new(1)
+        plane = Plane.new
+        plane1 = Plane.new
+        airport.land(plane)
+        expect (airport.land(plane1)).to raise_error "Airport Full!!"
+    end
+  end
+
   describe '#land' do
     it { is_expected.to respond_to :land }
 
     it 'lands a plane' do
-      airport = Airport.new
+      airport = Airport.new(1)
       plane = Plane.new
-      expect(airport.land(plane)).to eq plane
+      expect(airport.land(plane)).to eq [plane]
     end
   end
 
@@ -14,7 +25,7 @@ describe Airport do
     it { is_expected.to respond_to :takeoff }
 
     it 'takesoff a plane' do
-      airport = Airport.new
+      airport = Airport.new(1)
       plane = Plane.new
       expect(airport.takeoff(plane)).to eq "plane #{plane} has now left the airport "
     end
